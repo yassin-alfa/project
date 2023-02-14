e641bee1739197954b3e4ab8b55f5633fd243afe
yassin-alfa <yassin.al-fatni@supdevinci-edu.fr>
2023-02-14 14:39:32 +0100

    prof3

--- flask1.py
+++ flask1.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def get_employees():
     employees = [
         {'id': 1, 'firstName': 'Raouf', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
-        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+        {'id': 2, 'firstName': 'Amine', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
         {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
     ]
     return jsonify(employees)
