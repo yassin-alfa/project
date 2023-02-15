2f981b60f68b2b01d6b62a021e8c6d6b8275a0fb
yassin-alfa <yassin.al-fatni@supdevinci-edu.fr>
2023-02-15 09:55:21 +0100

    ajout du POST

--- flask1.py
+++ flask1.py
@@ -1,15 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+employees = [
+    {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+    {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+    {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+]
 
-@app.route('/alfat', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def get_employees():
-    employees = [
-        {'id': 1, 'firstName': 'Raouf', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
-        {'id': 2, 'firstName': 'Amine', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
-        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
-    ]
     return jsonify(employees)
+
+@app.route('/employees', methods=['POST'])
+def create_employee():
+    data = request.get_json()
+    new_employee = {
+        'id': len(employees) + 1,
+        'firstName': data['firstName'],
+        'lastName': data['lastName'],
+        'emailId': data['emailId']
+    }
+    employees.append(new_employee)
+    return jsonify(new_employee)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
