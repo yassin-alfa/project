e3b173cfb016c0177bfa0d5e096af05d9ecc0f38
yassin-alfa <125257257+yassin-alfa@users.noreply.github.com>
2023-02-16 14:52:52 +0100

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+       stage('clone github repo') {
+                steps {
+                    sh 'git clone https://github.com/raoufcherfa/employe.git'
+                }
+            }
+        stage('Build') {
+            steps {
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'pytest tests/'
+            }
+        }
+    }
+}
